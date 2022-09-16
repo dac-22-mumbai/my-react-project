54cfdf7987ffb6edc3156f826a119dde330253b3
santoshmondal <santosh.ece06@gmail.com>
2022-09-16 09:07:39 +0530

    b4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-// JSX - X :: XML
-// JSX - RETURNS HTML
-// JSX - MUST HAVE ROOT/PARENT TAG
 function App() {
   return (
     <div>
-      <h1>Hello Universe</h1>
-      <h1>Hello</h1>
+      <h1>Auto suggestion / Auto Formatt</h1>
     </div>
   );
 }
