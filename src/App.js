24757cee5d0f86cfb00629ac7f2871f11e2c73b7
santoshmondal <santosh.ece06@gmail.com>
2022-09-16 08:53:41 +0530

    b3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
-// JSX = (JAVASCRIPT + HTML)
+// JSX - X :: XML
+// JSX - RETURNS HTML
+// JSX - MUST HAVE ROOT/PARENT TAG
 function App() {
-  return <h1>Hello Universe</h1>;
+  return (
+    <div>
+      <h1>Hello Universe</h1>
+      <h1>Hello</h1>
+    </div>
+  );
 }
 
 export default App;
