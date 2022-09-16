fe8c7506cdf515272ac01d5b00d5ea8051331d3b
santoshmondal <santosh.ece06@gmail.com>
2022-09-16 09:30:06 +0530

    b6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 // INTERPOLATION
 function App() {
-  let prodcutTitle = "IPHONE 12";
+  let isIphone = false;
+  let prodcutTitleIphone = "IPHONE 12";
+  let productTitleAndorid = "Andorid 12";
   let productPrice = 99.99;
 
   // VIEW
   return (
     <div>
-      <h1> {prodcutTitle} </h1>
+      <h1> {isIphone ? "iphone" : "android"} </h1>
+
+      <h1> {isIphone ? prodcutTitleIphone : productTitleAndorid} </h1>
+
       <h1> {productPrice} </h1>
     </div>
   );
