e6d7ed10cb7713eb7cd915fb09a90856ef590559
santoshmondal <santosh.ece06@gmail.com>
2022-09-17 16:43:34 +0530

    b8

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
 import { useState } from "react";
 
 function App() {
-  // let message = "Hello World";
-  let [message, setMessage] = useState("Hello Universe");
+  let [list, setList] = useState([]);
 
-  // let count = 100;
-  let [count, setCount] = useState(100);
-
-  // let active = true;
-  let [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  let login = () => {
-    setIsLoggedIn(true);
-  };
-  let logout = () => {
-    setIsLoggedIn(false);
+  let addNewItem = () => {
+    const list1 = ["mumbai", ...list];
+    setList(list1);
   };
 
   return (
     <div>
       <h1>Hello World</h1>
+      <input type="button" value="Send" onClick={addNewItem} />
 
-      {isLoggedIn ? <h1>{message}</h1> : <h1>Please Login</h1>}
-      {isLoggedIn && <h1>{message}</h1>}
-
-      <h1>{count}</h1>
-
-      <input type="button" value="Login" onClick={login} />
-      <input type="button" value="Logout" onClick={logout} />
+      {list.map((item) => (
+        <h1>{item}</h1>
+      ))}
     </div>
   );
 }
