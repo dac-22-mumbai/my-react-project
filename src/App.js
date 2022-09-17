39280c40393fef6888244df33b367e257bff4afd
santoshmondal <santosh.ece06@gmail.com>
2022-09-17 16:48:01 +0530

    b9

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { useState } from "react";
 
 function App() {
+  // Stateful Variable
   let [list, setList] = useState([]);
 
+  // Event Handler
   let addNewItem = () => {
-    const list1 = ["mumbai", ...list];
+    const inputMesage = document.getElementById("id1").value;
+    const list1 = [inputMesage, ...list];
     setList(list1);
+
+    document.getElementById("id1").value = "";
   };
 
   return (
     <div>
       <h1>Hello World</h1>
+      <input type="text" id="id1" />
       <input type="button" value="Send" onClick={addNewItem} />
 
+      {/** WOrk with list/map */}
       {list.map((item) => (
         <h1>{item}</h1>
       ))}
