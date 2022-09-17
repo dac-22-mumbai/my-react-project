22faa65c66d6ea06f36d2fdbd4e77fbe11e710f6
santoshmondal <santosh.ece06@gmail.com>
2022-09-17 09:03:54 +0530

    b2

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
 function App() {
-  let list = ["Andorid", "Apple", "Samsumg"];
+  let id = "1";
+  let message = "Hello Universer";
 
   return (
-    <>
-      <ul>
-        {list.map((item) => (
-          <li>{item}</li>
-        ))}
-      </ul>
+    <div>
+      <h1>Interpolation at Attribute / Dynamic Attribute</h1>
+      <h1 id={id}>{message}</h1>
+      <h1>{message}</h1>
 
       <hr />
-      <div>
-        {list.map((item) => (
-          <h1>{item}</h1>
-        ))}
-      </div>
+      <h1 id="id1">{message}</h1>
+      <h1>{message}</h1>
 
       <hr />
-      <div>{list.map((item) => item)}</div>
-    </>
+      <h1>Hello World</h1>
+      <h1 id="h1">Hello World</h1>
+    </div>
   );
 }
-
-/**
- *  () -> {}
- *  () => {}
- */
 
 export default App;
