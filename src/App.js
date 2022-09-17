0bcbcca53ede03a3105c5fd8eb873d19004ae727
santoshmondal <santosh.ece06@gmail.com>
2022-09-17 09:13:18 +0530

    b3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 function App() {
-  let id = "1";
-  let message = "Hello Universer";
+  // Data Member
+  let message = "Hello";
+
+  // Member Function
+  let clickMe = () => {
+    alert();
+  };
 
   return (
     <div>
-      <h1>Interpolation at Attribute / Dynamic Attribute</h1>
-      <h1 id={id}>{message}</h1>
-      <h1>{message}</h1>
-
-      <hr />
-      <h1 id="id1">{message}</h1>
-      <h1>{message}</h1>
-
-      <hr />
-      <h1>Hello World</h1>
-      <h1 id="h1">Hello World</h1>
+      <h1>Event Handling</h1>
+      <input type="button" value="Click Me" id={message} onClick={clickMe} />
     </div>
   );
 }
