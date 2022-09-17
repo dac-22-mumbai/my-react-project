865124fe268a6aaa6aef02733ad4a9f8720fe655
santoshmondal <santosh.ece06@gmail.com>
2022-09-17 17:24:27 +0530

    react + bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-import "./App.css";
-
 function App() {
-  let style1 = { backgroundColor: "red", color: "white" };
-
   return (
     <div>
-      <h1 className="beautify">Hello World</h1>
-
-      <hr />
-      <h1 style={style1}>Hello World</h1>
-
-      <hr />
-      <h1 style={{ backgroundColor: "royalblue", color: "white" }}>
-        Hello World
-      </h1>
+      <h1 className="bg-primary text-light p-3">Hello World</h1>
     </div>
   );
 }
