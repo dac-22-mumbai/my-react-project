44566d7beb21ef86dc4741dfe91f1497d776532c
santoshmondal <santosh.ece06@gmail.com>
2022-09-17 09:19:52 +0530

    b4

--- src/App.js
+++ src/App.js
@@ -4,13 +4,36 @@ function App() {
 
   // Member Function
   let clickMe = () => {
-    alert();
+    alert("Hello World");
+  };
+
+  let clickMe1 = (p1) => {
+    alert(p1);
   };
 
   return (
     <div>
       <h1>Event Handling</h1>
-      <input type="button" value="Click Me" id={message} onClick={clickMe} />
+
+      {/** With PARAM */}
+      <input
+        type="button"
+        value="Click Android"
+        onClick={() => clickMe1("Android")}
+      />
+      <input
+        type="button"
+        value="Click iPhone"
+        onClick={() => clickMe1("iPhone")}
+      />
+      <input
+        type="button"
+        value="Click samsung"
+        onClick={() => clickMe1("Samsung")}
+      />
+
+      {/** SIMPLEST - NO PARAM */}
+      <input type="button" value="Click Me" onClick={clickMe} />
     </div>
   );
 }
