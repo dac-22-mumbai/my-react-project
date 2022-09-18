a1d1f561c16ba0e9a8112ac1148be3e52e28f1c2
santoshmondal <santosh.ece06@gmail.com>
2022-09-18 09:06:08 +0530

    small update.

--- src/App.js
+++ src/App.js
@@ -14,20 +14,10 @@ function App() {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="#link">About Us</Nav.Link>
+              <Nav.Link href="#link">Project</Nav.Link>
+              <Nav.Link href="#link">Product</Nav.Link>
+              <Nav.Link href="#link">Log Out</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
