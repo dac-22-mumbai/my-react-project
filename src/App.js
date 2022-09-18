152ca4e11201ba7ff099f55c219f2bdb01c2a922
santoshmondal <santosh.ece06@gmail.com>
2022-09-18 09:01:57 +0530

    react bootsrrap.

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
     <div>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Project Book</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
+            <Nav className="ms-auto">
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#link">Link</Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
