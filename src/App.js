94a5e6a59c5bdad8bb90c222fa518f7a88153f46
santoshmondal <santosh.ece06@gmail.com>
2022-09-18 09:00:06 +0530

    react bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,95 +1,119 @@
-import { useState } from "react";
+import { Accordion, Carousel } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 function App() {
-  let [validationError, setValidationError] = useState(false);
-  let [movieList, setMovieList] = useState([]);
-
-  let [movieName, setMovieName] = useState("");
-  let [movieReview, setMovieReview] = useState("");
-
-  let handleMovieNameChange = (e) => {
-    setMovieName(e.target.value);
-  };
-
-  let handleMovieReviewChange = (e) => {
-    setMovieReview(e.target.value);
-  };
-
-  let addMovieReview = () => {
-    console.log(movieName, movieReview);
-    if (movieReview.length < 150) {
-      setValidationError(true);
-
-      setTimeout(() => {
-        setValidationError(false);
-      }, 2500);
-      return;
-    }
-
-    let newMovieItem = { movieName: movieName, movieReview: movieReview };
-    let newMovieList = [newMovieItem, ...movieList];
-    setMovieList(newMovieList);
-
-    // debug purpose
-    console.log(newMovieList);
-
-    setMovieName("");
-    setMovieReview("");
-  };
-
   return (
     <div>
-      <h1 className="bg-success text-light p-3">Form Registration</h1>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-      <input
-        className="form-control my-1"
-        type="text"
-        placeholder="Movie Name"
-        value={movieName}
-        onChange={handleMovieNameChange}
-      />
-      <textarea
-        className="form-control my-1"
-        placeholder="Movie Review"
-        value={movieReview}
-        onChange={handleMovieReviewChange}
-      ></textarea>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://picsum.photos/400"
+            style={{ height: "400px", objectFit: "cover" }}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://picsum.photos/401"
+            style={{ height: "400px", objectFit: "cover" }}
+            alt="Second slide"
+          />
 
-      {validationError && (
-        <div className="alert alert-danger m-0 my-1">
-          Review Must have 150 chars.
-        </div>
-      )}
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://picsum.photos/402"
+            style={{ height: "400px", objectFit: "cover" }}
+            alt="Third slide"
+          />
 
-      <input
-        className="btn btn-outline-success w-100"
-        type="button"
-        value="Submit"
-        onClick={addMovieReview}
-      />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
 
-      <div className="row">
-        {movieList.map((item, index) => (
-          <div key={index} className="col-sm-12 col-md-4  my-1">
-            <div class="card">
-              <img
-                src={"https://picsum.photos/200" + index}
-                style={{ height: "200px", objectFit: "cover" }}
-                class="card-img-top"
-                alt="..."
-              />
-
-              <div class="card-body">
-                <h5 class="card-title text-capitalize">{item.movieName}</h5>
-                <p class="card-text">{item.movieReview}</p>
-                <a href="#" class="btn btn-outline-success">
-                  Read Full Review
-                </a>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+      <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="2">
+          <Accordion.Header>Accordion Item #3</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     </div>
   );
 }
