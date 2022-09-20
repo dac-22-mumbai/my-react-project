9ec3ec2f753cb6cd7963faf14cc3f107f901c3f6
santoshmondal <santosh.ece06@gmail.com>
2022-09-20 18:05:55 +0530

    b4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 function App() {
+  const [list, setList] = useState([]);
   const [user, setUser] = useState({
     username: "",
     password: "",
@@ -9,64 +10,80 @@ function App() {
   });
 
   const handleInputChange = (e) => {
-    // console.log(e.target.name, e.target.value);
-
-    // explain...
     const newuser = { ...user, [e.target.name]: e.target.value };
     setUser(newuser);
   };
 
   const handleSubmit = () => {
-    // task is save the form
     console.log(user);
+
+    // {...} / [...]
+    const newlist = [user, ...list];
+    setList(newlist);
+
+    // Clear the form
+    setUser({
+      username: "",
+      password: "",
+      email: "",
+      mobile: "",
+    });
   };
 
   return (
     <div>
       <h1>Forms Elements</h1>
 
-      <input
-        name="username"
-        className="form-control"
-        type="text"
-        placeholder="Enter Username"
-        value={user.username}
-        onChange={handleInputChange}
-      />
+      {/** FORM PARTS */}
+      <div>
+        <input
+          name="username"
+          className="form-control"
+          type="text"
+          placeholder="Enter Username"
+          value={user.username}
+          onChange={handleInputChange}
+        />
 
-      <input
-        name="password"
-        className="form-control"
-        type="password"
-        placeholder="Enter Passwrod"
-        value={user.password}
-        onChange={handleInputChange}
-      />
+        <input
+          name="password"
+          className="form-control"
+          type="password"
+          placeholder="Enter Passwrod"
+          value={user.password}
+          onChange={handleInputChange}
+        />
 
-      <input
-        name="email"
-        className="form-control"
-        type="email"
-        placeholder="Enter Email"
-        value={user.email}
-        onChange={handleInputChange}
-      />
+        <input
+          name="email"
+          className="form-control"
+          type="email"
+          placeholder="Enter Email"
+          value={user.email}
+          onChange={handleInputChange}
+        />
 
-      <input
-        name="mobile"
-        className="form-control"
-        type="mobile"
-        placeholder="Enter Mobile"
-        value={user.mobile}
-        onChange={handleInputChange}
-      />
+        <input
+          name="mobile"
+          className="form-control"
+          type="mobile"
+          placeholder="Enter Mobile"
+          value={user.mobile}
+          onChange={handleInputChange}
+        />
 
-      <input
-        className="btn btn-primary w-100 mt-2"
-        type="button"
-        value="Submit"
-        onClick={handleSubmit}
-      />
+        <input
+          className="btn btn-primary w-100 mt-2"
+          type="button"
+          value="Submit"
+          onClick={handleSubmit}
+        />
+      </div>
+
+      {/** DISPLAY THE LIST */}
+      {list.map((item, index) => (
+        <h1>{item.username}</h1>
+      ))}
     </div>
   );
 }
