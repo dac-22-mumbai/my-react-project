3cc14e2366439f8ef858ccbb38db6b9e2a808ce2
santoshmondal <santosh.ece06@gmail.com>
2022-09-20 18:13:11 +0530

    basic form.

--- src/App.js
+++ src/App.js
@@ -81,9 +81,28 @@ function App() {
       </div>
 
       {/** DISPLAY THE LIST */}
-      {list.map((item, index) => (
-        <h1>{item.username}</h1>
-      ))}
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Username</th>
+            <th scope="col">Password</th>
+            <th scope="col">Email</th>
+            <th scope="col">Mobile</th>
+          </tr>
+        </thead>
+        <tbody>
+          {list.map((item, index) => (
+            <tr key={index}>
+              <th scope="row">{index + 1}</th>
+              <td>{item.username}</td>
+              <td>{"*****"}</td>
+              <td>{item.email}</td>
+              <td>{item.mobile}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
