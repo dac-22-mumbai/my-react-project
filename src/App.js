ee781dd951da283bdb8cdaa52a00753a6a093fbd
santoshmondal <santosh.ece06@gmail.com>
2022-09-20 17:20:30 +0530

    single intput field.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 function App() {
+  const [username, setUsername] = useState("");
+
+  const handleChangeUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    // task is save the form
+    console.log(username);
+  };
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Forms Elements</h1>
+
+      <input
+        className="form-control"
+        type="text"
+        placeholder="Enter Username"
+        value={username}
+        onChange={handleChangeUsername}
+      />
+
+      <input
+        className="btn btn-primary w-100 mt-2"
+        type="button"
+        value="Submit"
+        onClick={handleSubmit}
+      />
     </div>
   );
 }
