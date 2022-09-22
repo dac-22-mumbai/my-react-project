235472b7e5f54094e3c7cf6965cd71c7066b4c32
santoshmondal <santosh.ece06@gmail.com>
2022-09-22 09:50:44 +0530

    b2

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-// Function => Component => TAG => User Define Tag
-// Java => Pre Define Clasess => String, Number, List
-//      => User Define Class => Person, Employee,...
-// HTML => Pre Define TAG => h1, h2, p, a, div,...
-// React=> User Define Tag => <Person /> <Employee />
-
-// THIS FUNCTION IS ALSO CREATING USER DEFINE TAG
-// <App />
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <h1>Heloo1</h1>
-      <h1>Heloo2</h1>
+      <h1>Hello I am Default</h1>
+
+      <h1>I am pre define tag</h1>
+
+      {/** AppHeader() */}
+      <AppHeader />
+
+      <AppHeader />
     </div>
   );
 }
 
-// IF FUNCTION RETURN JSX
-// IT BECOMES COMPONENT
-// IT BECOMES TAG
-// IT BECOMES USER DEFINE COMPONET
-// <Person />
-function Person() {
-  return <div>Hello</div>;
-}
+// <AppHeader />
+function AppHeader() {
+  console.log("Appheader method is called...");
 
-function Employee() {
-  return <div>Employee</div>;
+  return (
+    <div>
+      <h1>App Header</h1>
+    </div>
+  );
 }
 
 export default App;
