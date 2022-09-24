b32499bcf5cfe8498566f815956b89e0478afa6e
santoshmondal <santosh.ece06@gmail.com>
2022-09-24 10:12:39 +0530

    fake api.

--- src/components/Playground.js
+++ src/components/Playground.js
@@ -2,27 +2,32 @@ import axios from "axios";
 import { useState } from "react";
 
 function Playground() {
+  return (
+    <div>
+      <Playground1 />
+    </div>
+  );
+}
+
+function Playground1() {
   let [list, setList] = useState([]);
 
-  // MAKING API CALL
+  // this function is making call to backend
   const makeApiCall = async () => {
-    const url = `https://jsonplaceholder.typicode.com/posts`;
-    let res = await axios.get(url);
-    console.log(res);
+    const url = "https://jsonplaceholder.typicode.com/posts";
 
-    const newlist = res.data;
+    const response = await axios.get(url);
+    // console.log(response);
+
+    const newlist = response.data;
     setList(newlist);
   };
 
   return (
     <div>
-      <h1>Playgroun</h1>
+      <h1>Playground 1</h1>
 
-      <input
-        type="button"
-        value="MAKE API/AJAX/HTTP CALL"
-        onClick={makeApiCall}
-      />
+      <input type="button" value="Call Backend" onClick={makeApiCall} />
 
       {list.map((item, index) => (
         <div key={index} className="alert alert-success m-0 mb-1">
